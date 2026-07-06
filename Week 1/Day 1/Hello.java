833645f31ce0fade0e56cb8721ebb6fc4e260d02
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-06 20:37:34 +0500

    Week 1 Day 1 Tasks

---
+++ Week 1/Day 1/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args)
+    {
+        String fullName = "Fawad";
+        int age = 23;
+        long rollNumber = 11223344;
+        System.out.println("Your Name : " + fullName);
+        System.out.println("Your Age : " + age);
+        System.out.println("Your RollNo. : " + rollNumber);
+
+    }
+}
