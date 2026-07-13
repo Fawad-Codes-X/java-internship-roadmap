263d27794cf6323efdbfa991d4303ed9a9f73f22
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-13 18:47:28 +0500

    Classes Basics

---
+++ Week 2/Day 1/ClassesBasic.java
@@ -0,0 +1,21 @@
+public class ClassesBasic {
+
+    int employeeID;
+    String name;
+    String department;
+    double salary;
+
+    public ClassesBasic(int empID, String n, String dep, double sal){
+        employeeID = empID;
+        name = n;
+        department = dep;
+        salary = sal;
+    }
+
+    public static void main(String[] args){
+
+
+
+    }
+
+}
