e80998886d90412cc02f1ad7cfd7bb5f4e35786c
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-13 19:04:46 +0500

    Using constructor to initialize values and calling methods with objects

--- Week 2/Day 1/ClassesBasic.java
+++ Week 2/Day 1/ClassesBasic.java
@@ -1,19 +1,41 @@
 public class ClassesBasic {
 
-    int employeeID;
-    String name;
-    String department;
-    double salary;
+   public class Employee {
 
-    public ClassesBasic(int empID, String n, String dep, double sal){
-        employeeID = empID;
-        name = n;
-        department = dep;
-        salary = sal;
+        int employeeID;
+        String name;
+        String department;
+        double salary;
+
+        public Employee(int empID, String n, String dep, double sal) {
+            employeeID = empID;
+            name = n;
+            department = dep;
+            salary = sal;
+        }
+
+        public void displaySalary() {
+            System.out.println("Employee ID : " + employeeID);
+            System.out.println("Employee Name : " + name);
+            System.out.println("Employee Department : " + department);
+            System.out.println("Employee Salary : " + salary);
+
+        }
+
+        public void increaseSalary(){
+            salary = salary + 50000;
+        }
+
+
     }
 
     public static void main(String[] args){
 
+      ClassesBasic cb = new ClassesBasic();
+      Employee obj1 = cb.new Employee(1122, "Fawad", "IT", 100000);
+      obj1.displaySalary();
+      obj1.increaseSalary();
+      obj1.displaySalary();
 
 
     }
