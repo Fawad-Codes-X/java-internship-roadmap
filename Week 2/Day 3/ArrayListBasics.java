80857b9cfd341469a33820333b9a349e930baadb
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-14 22:35:25 +0500

    ArrayList Basics

---
+++ Week 2/Day 3/ArrayListBasics.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ArrayListBasics {
+
+
+
+    public static void main(String[] args){
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+
+        System.out.println(fruits);
+
+    }
+
+}
