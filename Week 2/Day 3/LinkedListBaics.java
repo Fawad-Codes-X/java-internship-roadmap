a3c08d73a41d6022378ac19fdbddf8c6a2ab9882
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-14 23:29:29 +0500

    LinkedList Basics

---
+++ Week 2/Day 3/LinkedListBaics.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class LinkedListBaics {
+
+    public static void main(String[] args){
+
+        LinkedList <String> car = new LinkedList<>();
+
+        car.add("Bugatti");
+        car.add("Audi");
+        car.add("BMW");
+
+        System.out.println(car);
+
+    }
+
+}
