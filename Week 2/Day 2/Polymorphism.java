071950aa15bc8534b84f8b8693a583a551923866
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-14 19:59:13 +0500

    Method Overloading & Overriding Implementation

--- Week 2/Day 2/Polymorphism.java
+++ Week 2/Day 2/Polymorphism.java
@@ -5,8 +5,8 @@ public class Polymorphism {
         protected int age;
 
         public Person(String name, int age){
-            this.name;
-            this.age;
+            this.name = name;
+            this.age = age;
             System.out.println("Parent Constructor Called.");
         }
 
@@ -27,9 +27,44 @@ public class Polymorphism {
             this.salary = salary;
         }
 
+
+        //Method Overrding
+        @Override
+        public void displayInfo() {
+            super.displayInfo();
+            System.out.println("Employee ID : " + employeeId);
+            System.out.println("Employee Salary : " + salary);
+        }
+
+
+        //Method Overloading
+        public double calculateSalary(){
+            return salary;
+        }
+
+        public double calculateSalary(double bonus){
+            return salary + bonus;
+        }
+
+        public double calculateSalary(double bonus, double tax){
+            return (salary + bonus) - tax;
+        }
+
     }
 
     public static void main(String[] args){
+
+        Person person = new Employee("Fawad", 22, 1102, 100000.0);
+
+        System.out.println("\n===== Display Info =====");
+        person.displayInfo();
+
+        Employee emp = (Employee) person;
+
+        System.out.println("\n===== Salary Calculations =====");
+        System.out.println("Salary : " + emp.calculateSalary());
+        System.out.println("Salary After Bonus : " + emp.calculateSalary(5000));
+        System.out.println("Salary After Bonus & Tax : " + emp.calculateSalary(5000, 2000));
 
     }
 
