14e1df3c3def7cedb4bce758cdd79a78c2599ef3
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-15 18:26:08 +0500

    HashMap Implementation

---
+++ Week 2/Day 4/HashMapImplementation.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class HashMapImplementation {
+
+
+
+    public static void main(String[] args){
+
+        HashMap<String, Integer> = new HashMap<>();
+
+    }
+
+}
