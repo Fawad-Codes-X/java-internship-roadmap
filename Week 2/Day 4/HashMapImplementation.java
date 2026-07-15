62390515ef07eb39b43627b0e3ba1669a2e284d1
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-15 18:53:01 +0500

    Hashmap Operation

--- Week 2/Day 4/HashMapImplementation.java
+++ Week 2/Day 4/HashMapImplementation.java
@@ -6,7 +6,17 @@ public class HashMapImplementation {
 
     public static void main(String[] args){
 
-        HashMap<String, Integer> = new HashMap<>();
+        HashMap<String, Integer> country = new HashMap<>();
+        country.put("Pakistan", 130);
+        country.put("India", 90);
+        country.put("China", 100);
+        System.out.println(country);
+
+        if(country.containsKey("China")){
+            System.out.println("Key Exists in map...");
+        }else {
+            System.out.println("Key Doesn't Exist In The Map");
+        }
 
     }
 
