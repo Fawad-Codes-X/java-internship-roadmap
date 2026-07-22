c1ff30fdc480e2cfa95736f1ad77a06924be5ec9
fawad.bscs@gmail.com <fawad.bscs@gmail.com>
2026-07-22 21:30:18 +0500

    Calculator using lambda expression and functional interface mini Task

---
+++ Week3/Day3/CalculatorClass.java
@@ -0,0 +1,33 @@
+package Week3.Day3;
+
+public class CalculatorClass {
+
+    @FunctionalInterface
+    interface CalculatorOperation{
+        double calculate(double a , double b);
+    }
+
+    public static void main(String[] args){
+
+        CalculatorOperation add = (a,b) -> a + b;
+
+        CalculatorOperation subtract = (a,b) -> a - b;
+
+        CalculatorOperation multiply = (a, b) -> a * b;
+
+        CalculatorOperation division = (a,b) -> {
+            if(b==0){
+                System.out.println("Cannot Divide By Zero.");
+                return 0;
+            }
+            return a /b;
+        };
+
+        System.out.println("Addition : " + add.calculate(10, 20));
+        System.out.println("Subtraction : " + subtract.calculate(40,20));
+        System.out.println("Multiplication : " + multiply.calculate(10, 20));
+        System.out.println("Division : " + division.calculate(40, 2));
+
+    }
+
+}
